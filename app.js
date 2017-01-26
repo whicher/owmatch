31366410190c260e324b62fdc826e827939016a8
hakanu <hakan_uysal@ymail.com>
2017-01-26 17:34:46 +0000

    Initial commit.

---
+++ app.js
@@ -0,0 +1,52 @@
+var _TOKEN_URL = 'https://eu.api.battle.net/account/user?access_token=';
+var _SERVER_DOMAIN = 'localhost:5000';
+var _API_PATH = 'http://' + _SERVER_DOMAIN + '/api/get_token/';
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var currentUrl = window.location.href;
+    console.log('currentUrl: ' + currentUrl);
+    var params = getJsonFromUrl(currentUrl);
+
+    // Handle authorization.
+    if (params != null) {
+      console.log(params);
+      console.log(params['code']);
+
+      var authCode = params['code'];
+      // Request the access token.
+      if (params['code']) {
+        $.ajax({
+          url: _API_PATH + authCode,
+          method: 'GET',
+          success: function (data) {
+            console.log('success: ');
+            console.log(data);
+            if (data.token_type) {
+              $.ajax({
+                url: _TOKEN_URL + data.access_token,
+                method: 'GET',
+                success: function (data) {
+                  console.log('success: ');
+                  console.log(data);
+                  $('#p-login').hide();
+                  $('#p-battletag').text('Logged in as ' + data.battletag);
+                }
+              });
+            }
+          }
+        });
+      }
+    }
+});
+
+function getJsonFromUrl() {
+  console.log('getJsonFromUrl');
+  var query = location.search.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
