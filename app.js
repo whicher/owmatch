84f076b54f9054d35a7c33201b38ec58630d5cc4
Haku <info@hakanu.net>
2017-01-26 23:30:45 +0000

    Make prod api server https.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var _TOKEN_URL = 'https://eu.api.battle.net/account/user?access_token=';
 var _DEV_SERVER_DOMAIN = 'localhost:5000';
-var _PROD_SERVER_DOMAIN = '89.100.176.29:7070';
+var _PROD_SERVER_DOMAIN = 'api.owmatch.me';
 var _API_PATH = 'http://' + _PROD_SERVER_DOMAIN + '/api/get_token/';
 
 $( document ).ready(function() {
